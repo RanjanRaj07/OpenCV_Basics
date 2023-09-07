5a3496140c5126ca1d3deed65314329ae7266ec5
Ranjan G <119508085+RanjanRaj07@users.noreply.github.com>
2023-09-07 09:44:44 +0530

    Update cv_camerAccess.py

--- cv_camerAccess.py
+++ cv_camerAccess.py
@@ -3,19 +3,24 @@ import sys
 
 s = 0
 
+#for multiple camera devices giving the camera ID by command line argument 
 if len(sys.argv) > 1:
     s = sys.argv[1]
 
+# Capture the video frames
 source = cv2.VideoCapture(s)
-win_name = 'Camera Preview'
 
+# Open a window to display the captured frames
+win_name = 'Camera Preview'
 cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
 
+# Show the window data till the user presses the ESC key
 while cv2.waitKey(1) != 27:
     has_frame, frame = source.read()
     if not has_frame:
         break
     cv2.imshow(win_name, frame)
 
+# Release the source and destroy the window
 source.release()
 cv2.destroyWindow(win_name)
